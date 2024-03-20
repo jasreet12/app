2ba1c137f422b244a994d0d4b72b1d318b0e08e7
Glitch (glitch-hello-website) <none>
2024-03-20 05:41:13 +0000

    🏛🏏 Checkpoint ./templates/index.html:62612849/90 ./app.py:62612849/6

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def method1():
   return render_template("index.html")
-  return " Welcome AIML Jan24 f1"
+  # return " Welcome AIML Jan24 f1"
 
 if __name__ == '__main__':
   app.run()
