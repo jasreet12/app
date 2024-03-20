98e4fabda6db9133c0fa418ed207ba9ac7734938
Glitch (glitch-hello-website) <none>
2024-03-20 06:29:14 +0000

    🍏⚓️ Checkpoint ./requirements.txt:62612849/26 ./app.py:62612849/1876 ./templates/index.html:62612849/10

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 from flask import *
- 
+import pandas as pd 
+import numpy as np 
+from sklearn.linear_model import LinearRegression
+
 app = Flask(__name__)
-###########
-file = "https://github.com/sarwansingh/Python/blob/master/ClassExamples/data/Bengaluru_House_Data_clean.csv"
+##############
+file ="https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/Bengaluru_House_Data_clean.csv"
+df = pd.read_csv(file)
+X = df.drop(['Unnamed: 0' ,'price'], axis='columns')
+Y = df.price
 
+lrmodel = LinearRegression()
+lrmodel.fit(X,Y)
+def predictprice(location,sqft,bath,bhk):
+  '''
+  Function which helps to actually predict the prices.
 
-###########
+  '''
+  loc_index = np.where(X.columns==location)[0][0] # np.where() function returns the indices of elements in an input array where the given condition is satisfied.
+
+  x = np.zeros(len(X.columns)) # np.zeros() function returns a new array of given shape and type, with zeros.
+  x[0] = sqft
+  x[1] = bath
+  x[2] = bhk
+  if loc_index >= 0:
+    x[loc_index] = 1
+  return lrmodel.predict([x])[0]
+############
 
 @app.route('/')
 def method1():
-  return render_template("index.html")
-  # return " Welcome AIML Jan24 f1"
+  pp = predictprice('Kothanur', 1500, 3, 3).round(3)
+  return render_template("index.html" , pprice=pp)
+
 @app.route('/contact')
-def method_contact():
+def methodcontact():
   return render_template("contact.html")
+
+# return " Welcome AIML Jan24 f1"
 
 if __name__ == '__main__':
   app.run()
