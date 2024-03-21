338fb0f9a6426ffa161d73c3617a91af1a7e0388
Glitch (glitch-hello-website) <none>
2024-03-21 05:00:59 +0000

    🐺🦄 Checkpoint ./templates/userInput.html:62612849/1643 ./app.py:62612849/3432

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 from sklearn.linear_model import LinearRegression
 
-app = Flask(__name__)
+app = Flask(_name_)
 ##############
 file ="https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/Bengaluru_House_Data_clean.csv"
 df = pd.read_csv(file)
@@ -13,54 +13,44 @@ Y = df.price
 lrmodel = LinearRegression()
 lrmodel.fit(X,Y)
 def predictprice(location,sqft,bath,bhk):
-  '''
-  Function which helps to actually predict the prices.
+    '''
+    Function which helps to actually predict the prices.
+    '''
+    loc_index = np.where(X.columns==location)[0][0]     # np.where() function returns the indices of elements in an input array where the given condition is satisfied.
 
-  '''
-  loc_index = np.where(X.columns==location)[0][0] # np.where() function returns the indices of elements in an input array where the given condition is satisfied.
+    x = np.zeros(len(X.columns))    # np.zeros() function returns a new array of given shape and type, with zeros.
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index >= 0:
+        x[loc_index] = 1
 
-  x = np.zeros(len(X.columns)) # np.zeros() function returns a new array of given shape and type, with zeros.
-  x[0] = sqft
-  x[1] = bath
-  x[2] = bhk
-  if loc_index >= 0:
-    x[loc_index] = 1
-  return lrmodel.predict([x])[0]
+    return lrmodel.predict([x])[0]
 ############
 
 @app.route('/')
 def method1():
-  pp = predictprice('Kothanur', 1300, 3, 3).round(3)
-  return render_template("index.html" , pprice=pp)
+  
+  return render_template("index.html" )
+
+@app.route('/project')
+def methodproject():
+  return render_template("project.html")
+
+@app.route('/predict' ,methods =["POST"] )
+def methodpredict():
+  loc   = request.form.get("loc")  #'Kothanur'
+  sqft  = request.form.get("size")
+  bhk   = request.form.get("bhk")
+  bath  = request.form.get("bath")
+  pp = predictprice(loc, sqft, bhk, bath).round(3)
+  return render_template("project.html", pprice=pp)
 
 @app.route('/contact')
 def methodcontact():
   return render_template("contact.html")
 
-@app.route('/userInput', methods =["GET", "POST"])
-def methoduserInput():
-  return render_template("userInput.html")
-
-@app.route('/userInput', methods =["GET", "POST"])
-def methoduserInput():
-  if request.method == "POST":
-       # getting input with name = fname in HTML form
-       loc = request.form.get("location")
-       # getting input with name = lname in HTML form 
-       size = request.form.get("size") 
-       bhk = request.form.get("bhk") 
-       bathroom = request.form.get("broom")
-      
-       pp = predictprice(loc, size, bhk, bathroom).round(3)
-  
-  
-  return render_template("userInput.html", predictPrice = pp)
-
-
-
 # return " Welcome AIML Jan24 f1"
 
-if __name__ == '__main__':
+if _name_ == '_main_':
   app.run()
-  
-  
