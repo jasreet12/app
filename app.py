252cc2afb922c5fff4c341d5589257aaa860eb96
Glitch (glitch-hello-website) <none>
2025-07-10 15:05:07 +0530

    House Price Prediction
    First Update

--- app.py
+++ app.py
@@ -1,142 +1,52 @@
-from flask import *
-import pandas as pd 
-import numpy as np 
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
-import sqlite3
-from flask import request
-from datetime import datetime
 
 app = Flask(__name__)
 
-
-def create_database():
-  # Connect to the SQLite database (or create it if it doesn't exist)
-  conn = sqlite3.connect('myDB')
-
-    # Create a cursor object to execute SQL commands
-  cur = conn.cursor()
-
-    # Define SQL commands to create tables
-  cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-  # Fetch all table names
-  tables = cur.fetchall()
-
-  # Print table names
-  print("Tables in the database:")
-  for table in tables:
-      print(table[0])
-    
-
-  # Close the connection
-  conn.close()
-# create_database()
-
-
-
-# house price prediction -1st part
-##############
-file ="https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/Bengaluru_House_Data_clean.csv"
+# 📦 Load & Train Model
+file = "https://raw.githubusercontent.com/sarwansingh/Python/master/ClassExamples/data/Bengaluru_House_Data_clean.csv"
 df = pd.read_csv(file)
-X = df.drop(['Unnamed: 0' ,'price'], axis='columns')
-Y = df.price
+
+X = df.drop(['Unnamed: 0', 'price'], axis=1)
+y = df['price']
 
 lrmodel = LinearRegression()
-lrmodel.fit(X,Y)
+lrmodel.fit(X, y)
 
-#1st part end
-######### get all locations in the list
-# collist = df.columns
-# loclist = []
-# for item in collist[5:] : 
-# # print (" <option> " + item + "</option>")
-# loclist.append( " <option> " + item + "</option>" )
-##################
+# 🧠 Prediction Function
+def predict_price(location, sqft, bath, bhk):
+    try:
+        loc_index = np.where(X.columns == location)[0][0]
+    except:
+        loc_index = -1
 
+    x = np.zeros(len(X.columns))
+    x[0] = float(sqft)
+    x[1] = float(bath)
+    x[2] = float(bhk)
 
+    if loc_index >= 0:
+        x[loc_index] = 1
 
-@app.route('/showdata')
-def showdata():
-    con = sqlite3.connect("myDB")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("SELECT id,name, strftime('%Y-%m-%d %H:%M:%S', date_time) AS formatted_date_time FROM attendance")
-    rows = cur.fetchall()
-    rows_with_serial = [(idx + 1, row['name'], row['formatted_date_time']) for idx, row in enumerate(rows)]
-    con.close()
-    return render_template("showData.html", data=rows_with_serial)
+    return round(lrmodel.predict([x])[0], 3)
 
-######
-
-
-
-
-
-@app.route('/adduserdata1', methods=['get'])  # Specify that this route handles POST requests
-def adduserdata1():
-    name = request.form.get("name")
-    con = sqlite3.connect("myDB")  
-    con.row_factory = sqlite3.Row  
-    cur = con.cursor()             
-  
-    insql = "INSERT INTO attendance(name, date_time) VALUES (?, datetime('now', '+5 hours', '+30 minutes'))"
-    cur.execute(insql, (name,))
-    con.commit() 
-    con.close()
-  
-    return render_template('index.html')
-
-# house price prediction -2nd part
-
-def predictprice(location,sqft,bath,bhk):
-  '''
-  Function which helps to actually predict the prices.
-  '''
-  loc_index = np.where(X.columns==location)[0][0] # np.where() function returns the indices of elements in an input array where the given condition is satisfied.
-
-  x = np.zeros(len(X.columns)) # np.zeros() function returns a new array of given shape and type, with zeros.
-  x[0] = sqft
-  x[1] = bath
-  x[2] = bhk
-  if loc_index >= 0:
-    x[loc_index] = 1
-
-  return lrmodel.predict([x])[0]
-############
-
+# 🔹 Home Page
 @app.route('/')
-def method1():
-  
-  return render_template("index.html" )
+def home():
+    return render_template("index.html", locations=X.columns[3:], pprice=None)
 
-@app.route('/project')
-def methodproject():
-  return render_template("project.html" , locations = df.columns[5:])
+# 🔹 Predict Route
+@app.route('/predict', methods=["POST"])
+def predict():
+    loc = request.form.get("loc")
+    sqft = request.form.get("size")
+    bhk = request.form.get("bhk")
+    bath = request.form.get("bath")
+    predicted_price = predict_price(loc, sqft, bath, bhk)
+    return render_template("index.html", pprice=predicted_price, locations=X.columns[3:])
 
-@app.route('/predict' ,methods =["POST"] )
-def methodpredict():
-  loc = request.form.get("loc") #'Kothanur'
-  sqft = request.form.get("size")
-  bhk = request.form.get("bhk")
-  bath = request.form.get("bath")
-  pp = predictprice(loc, sqft, bath, bhk).round(3)
-  return render_template("project.html", pprice=pp ,locations = df.columns[5:] )
-
-# 2nd part end
-
-
-@app.route('/contact')
-def methodcontact():
-  return render_template("contact.html")
-
-
-
-@app.route('/getlocation')
-def methodLocation():
-  return render_template("getLocation.html")
-
-
-
-# return " Welcome AIML Jan24 f1"
-
+# ▶️ Run App
 if __name__ == '__main__':
-  app.run()
+    app.run(debug=True)
